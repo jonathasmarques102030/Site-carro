6cab2d9fe08ed00d219832b3f1073537b7903483
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2022-08-15 00:48:56 -0300

    ejs

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3333
 
-app.set("view engine")
+app.use(express.static("/public"));
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("page/pagina-inicial")
+})
+
+app.get("/login", (req, res) => {
+    res.render("page/login")
+})
 
 app.listen(port, () => {
     console.log(`Servidor iniciado: ${port}`);
 });
+
